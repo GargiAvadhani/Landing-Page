87c240209fe4edaee116ba920f5426fa3bf32347
gargiavadhani@gmail.com <gargiavadhani@gmail.com>
2024-03-18 00:27:02 +0530

    secondcommit

--- src/Bar.jsx
+++ src/Bar.jsx
@@ -13,6 +13,7 @@ const Bar = () => {
         document.querySelector("nav").classList.toggle(styles.open);
         document.body.classList.toggle("lockScroll");
     }
+    
 
     return (
         <div className={styles.top_bar}>

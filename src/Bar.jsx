b0fbb3a2fc3ed4949dd1a298bb255ccb6bf77afb
gargiavadhani@gmail.com <gargiavadhani@gmail.com>
2024-03-18 00:31:14 +0530

    3rdcommit

--- src/Bar.jsx
+++ src/Bar.jsx
@@ -13,13 +13,13 @@ const Bar = () => {
         document.querySelector("nav").classList.toggle(styles.open);
         document.body.classList.toggle("lockScroll");
     }
-    
+
 
     return (
         <div className={styles.top_bar}>
             <img 
             src="images/logo2 (2).png" 
-            alt="insure logo"
+            alt="netsensor-logo"
             />
             <button className={styles.menu_btn} onClick={(e) => {handleClick(e)}}>
             <img 
